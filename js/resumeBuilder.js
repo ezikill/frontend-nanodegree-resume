abe62a3238749a0c47ef2d8e2f55d194117dc82c
Colin McKean <ezikill@gmail.com>
2016-09-20 14:36:57 +0100

    add name and role

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,10 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+$("#main").append(["Colin"]);
+
+var formattedName = HTMLheaderName.replace("%data%", "Colin McKean");
+var formattedRole = HTMLheaderRole.replace("%data%", "Developer");
+
+$("#header").append(formattedName);
+$("#header").append(formattedRole);
